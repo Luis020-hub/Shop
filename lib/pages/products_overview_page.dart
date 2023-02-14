c528f750e470f2ca973c0c98f37727c98f4d14d8
Luis020-hub <guhmonteiro10@hotmail.com>
2023-02-14 15:33:23 -0300

    14/02/2023
    - Produtos adicionados.
    - Estrutura inicial da pagina de produtos.

---
+++ lib/pages/products_overview_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:shop/data/dummy_data.dart';
+import 'package:shop/models/product.dart';
+
+class ProductsOverviewPage extends StatelessWidget {
+  ProductsOverviewPage({super.key});
+
+  final List<Product> loadedProducts = dummyProducts;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          'My Shop',
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: GridView.builder(
+          itemCount: loadedProducts.length,
+          itemBuilder: (ctx, i) => Text(loadedProducts[i].title),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+          ),
+        ),
+      ),
+    );
+  }
+}
