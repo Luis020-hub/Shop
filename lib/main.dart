9221c90110e6c18ad6b7ca54074c2fbfcbd77aa2
Luis020-hub <guhmonteiro10@hotmail.com>
2023-02-16 17:50:56 -0300

    16/02/2023
    - Usando o Provider.
    - Criando o modelo product_list.dart.
    - Usando o Provider no product overview.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/product_list.dart';
 import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/pages/products_overview_page.dart';
 import 'package:shop/utils/app_rotues.dart';
@@ -11,22 +13,25 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch(
-          primarySwatch: Colors.deepPurple,
-        ).copyWith(
-          secondary: Colors.redAccent,
-          onSecondary: Colors.amber
+    return ChangeNotifierProvider(
+      create: (_) => ProductList(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch(
+            primarySwatch: Colors.deepPurple,
+          ).copyWith(
+            secondary: Colors.redAccent,
+            onSecondary: Colors.amber,
+          ),
+          fontFamily: 'Lato',
         ),
-        fontFamily: 'Lato',
+        home: ProductsOverviewPage(),
+        routes: {
+          AppRoutes.productDetail: (ctx) => const ProductDetailPage(),
+        },
+        debugShowCheckedModeBanner: false,
       ),
-      home: ProductsOverviewPage(),
-      routes: {
-        AppRoutes.productDetail: (ctx) => ProductDetailPage()
-      },
-      debugShowCheckedModeBanner: false,
     );
   }
 }
