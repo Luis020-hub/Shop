7073ffd0a4362e9048eb5b1e8511b808fc3ae4dc
Luis020-hub <guhmonteiro10@hotmail.com>
2023-02-26 18:48:06 -0300

    26/02/2023
    - Adicionando a parte de url.

--- lib/pages/product_form_page.dart
+++ lib/pages/product_form_page.dart
@@ -12,12 +12,14 @@ class _ProductFormPageState extends State<ProductFormPage> {
   Widget build(BuildContext context) {
     final _priceFocus = FocusNode();
     final _descriptionFocus = FocusNode();
+    final _imageUrlFocus = FocusNode();
 
     @override
     void dispose() {
       super.dispose();
       _priceFocus.dispose();
       _descriptionFocus.dispose();
+      _imageUrlFocus.dispose();
     }
 
     return Scaffold(
@@ -33,13 +35,14 @@ class _ProductFormPageState extends State<ProductFormPage> {
               TextFormField(
                 decoration: const InputDecoration(labelText: 'Name'),
                 textInputAction: TextInputAction.next,
+                focusNode: _priceFocus,
                 onFieldSubmitted: (_) {
                   FocusScope.of(context).requestFocus(_priceFocus);
                 },
               ),
               TextFormField(
                 decoration: const InputDecoration(labelText: 'Price'),
-                focusNode: _priceFocus,
+                focusNode: _descriptionFocus,
                 keyboardType: const TextInputType.numberWithOptions(
                   decimal: true,
                 ),
@@ -49,10 +52,35 @@ class _ProductFormPageState extends State<ProductFormPage> {
               ),
               TextFormField(
                 decoration: const InputDecoration(labelText: 'Description'),
-                textInputAction: TextInputAction.next,
                 focusNode: _descriptionFocus,
+                textInputAction: TextInputAction.next,
                 keyboardType: TextInputType.multiline,
                 maxLines: 3,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: const InputDecoration(labelText: 'Url image'),
+                      keyboardType: TextInputType.url,
+                      textInputAction: TextInputAction.done,
+                      focusNode: _imageUrlFocus,
+                    ),
+                  ),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    margin: const EdgeInsets.only(top: 15, left: 15),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey, width: 1),
+                    ),
+                    alignment: Alignment.center,
+                    child: const Text(
+                      'Enter the URL',
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
