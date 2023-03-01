1fa87ecd0fba8d215f2d46f1e5e20ed0bdb8fa56
Luis020-hub <guhmonteiro10@hotmail.com>
2023-03-01 18:31:22 -0300

    01/03/2023
    - Tratamento de erro para não ficar no loading infinito.

--- lib/pages/product_form_page.dart
+++ lib/pages/product_form_page.dart
@@ -85,7 +85,21 @@ class _ProductFormPageState extends State<ProductFormPage> {
     Provider.of<ProductList>(
       context,
       listen: false,
-    ).saveProduct(_formData).then(
+    ).saveProduct(_formData).catchError((error) {
+      return showDialog<void>(
+        context: context,
+        builder: (ctx) => AlertDialog(
+          title: const Text('An error has occurred'),
+          content: const Text('An error occurred while trying to save a product'),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text('Ok'),
+            )
+          ],
+        ),
+      );
+    }).then(
       (value) {
         setState(() => _isLoading = false);
         Navigator.of(context).pop();
