acd8ac204a50c01a5652635a46166c567be33881
Luis020-hub <guhmonteiro10@hotmail.com>
2023-03-01 23:45:14 -0300

    01/03/2023
    - Importando os produtos do firebase.
    - If adicionado no product_list.dart para parar o loading infinito que acontecia quando não tem nenhum item cadastrado na loja.

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -3,14 +3,13 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:shop/data/dummy_data.dart';
 import 'package:shop/models/product.dart';
 
 class ProductList with ChangeNotifier {
   final _url =
       'https://shop-flutter-41b4d-default-rtdb.firebaseio.com/products.json';
 
-  final List<Product> _items = dummyProducts;
+  final List<Product> _items = [];
 
   List<Product> get items => [..._items];
   List<Product> get favoriteItems =>
@@ -22,7 +21,21 @@ class ProductList with ChangeNotifier {
 
   Future<void> loadProducts() async {
     final response = await http.get(Uri.parse(_url));
-    print(jsonDecode(response.body));
+    if (response.body == 'null') return;
+    Map<String, dynamic> data = jsonDecode(response.body);
+    data.forEach((productId, productData) {
+      _items.add(
+        Product(
+          id: productId,
+          name: productData['name'],
+          description: productData['description'],
+          price: productData['price'],
+          imageUrl: productData['imageUrl'],
+          isFavorite: productData['isFavorite'],
+        ),
+      );
+    });
+    notifyListeners();
   }
 
   Future<void> saveProduct(Map<String, Object> data) {
