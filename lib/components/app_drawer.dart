9549f64955572bc00f5e984e39abc62e728be2e9
Luis020-hub <guhmonteiro10@hotmail.com>
2023-03-14 20:43:19 -0300

    14/03/2023
    - Criado o drawer de Log out e navegando para a tela principal da aplicação ao clicar.
    - Em auth.dart foi criado o método para fazer o log out.

--- lib/components/app_drawer.dart
+++ lib/components/app_drawer.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../models/auth.dart';
 import '../utils/app_routes.dart';
 
 class AppDrawer extends StatelessWidget {
@@ -41,6 +43,21 @@ class AppDrawer extends StatelessWidget {
             onTap: () {
               Navigator.of(context).pushReplacementNamed(
                 AppRoutes.products,
+              );
+            },
+          ),
+          const Divider(),
+          ListTile(
+            leading: const Icon(Icons.exit_to_app),
+            title: const Text('Log out'),
+            onTap: () {
+              Provider.of<Auth>(
+                context,
+                listen: false,
+              ).logout();
+
+              Navigator.of(context).pushReplacementNamed(
+                AppRoutes.auth_or_home,
               );
             },
           ),
