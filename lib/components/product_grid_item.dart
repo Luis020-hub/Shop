06879fd15dec6eea0b339131217d00e74613ed90
Luis020-hub <guhmonteiro10@hotmail.com>
2023-03-18 21:29:41 -0300

    18/03/2023
    - Usando o FadeInImage nos produtos caso não sejam carregados na hora.
    - Criado a pasta Image em Assets.

--- lib/components/product_grid_item.dart
+++ lib/components/product_grid_item.dart
@@ -20,10 +20,15 @@ class ProductGridItem extends StatelessWidget {
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         child: GestureDetector(
-          child: Image.network(
-            product.imageUrl,
+          child: FadeInImage(
+            placeholder: const AssetImage('assets/images/31351.png'),
+            image: NetworkImage(product.imageUrl),
             fit: BoxFit.cover,
           ),
+          // child: Image.network(
+          //   product.imageUrl,
+          //   fit: BoxFit.cover,
+          // ),
           onTap: () {
             Navigator.of(context).pushNamed(
               AppRoutes.productDetail,
