ba41d0a5e4cd2dfa826b1a8e50afab304b693b91
Luis020-hub <guhmonteiro10@hotmail.com>
2023-03-29 15:23:00 -0300

    29/03/2023

--- lib/utils/constants.dart
+++
@@ -1,8 +0,0 @@
-class Constants {
-    static const user_favorite_url =
-      'https://shop-flutter-41b4d-default-rtdb.firebaseio.com/userFavorite';
-  static const productBaseUrl =
-      'https://shop-flutter-41b4d-default-rtdb.firebaseio.com/products';
-    static const orderBaseUrl =
-      'https://shop-flutter-41b4d-default-rtdb.firebaseio.com/orders';
-}
